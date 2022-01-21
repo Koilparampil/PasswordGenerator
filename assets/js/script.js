f582f41c4715b9a0eb66712dbc8cf429e4288115
KreshsDesktop <ckresh99@gmail.com>
2022-01-20 19:21:35 -0500

    added input validation for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,27 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword(){
+  //Asking for length of the password
   numChar=window.prompt('How many Characters do you want your Password to be?');
-  console.log(numChar);
-  if (typeof numChar != Number){
+  //converting to a Number instead of a string
+  numChar=parseInt(numChar);
+  //Checking if a valid number has been entered
+  console.log(typeof numChar);
+  if (isNaN(numChar)){
     window.alert("You have not entered a Number")
+    return
   }
+//Asking if the user wants symbols in their passwords
   SymbolC=window.confirm("Do you want Symbols in your Password?")
   if (SymbolC){
     //add code for password with Symbols
     console.log("They want Symbols")
+   
   }
   else{
     //Add code for passsword without symbols 
     console.log("They don't want Symbols")
+  
   }
 }
 
