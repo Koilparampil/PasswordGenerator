4a4a7f8bda2b40f7d331d08aa434699d2a3d32b5
KreshsDesktop <ckresh99@gmail.com>
2022-01-20 19:22:45 -0500

    added symbol password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
+function actualPasswordS(numofPass){
+  CharsS=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", 
+  "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+   "r", "s", "t", "u", "v", "w", "x", "y", "z",'1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','&',
+   '*','(',')','[',']',';',',','.','/','`','_','-','=','+','{','}','~','"','|']
+SymbolPassS=""
+for(var i=0; i<=numofPass; i++){
+  SymbolPassS=SymbolPassS+CharsS[Math.floor(Math.random() * CharsS.length) ]
+}
+console.log(SymbolPassS);
+return SymbolPassS;
+}
+
+
+
+
 function generatePassword(){
   //Asking for length of the password
   numChar=window.prompt('How many Characters do you want your Password to be?');
