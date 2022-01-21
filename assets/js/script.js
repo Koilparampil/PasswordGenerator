643af595ada0cd96f0784492d10fab43d29357de
KreshsDesktop <ckresh99@gmail.com>
2022-01-20 19:24:06 -0500

    added symbolless  password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,17 @@ for(var i=0; i<=numofPass; i++){
 console.log(SymbolPassS);
 return SymbolPassS;
 }
-
+function actualPasswordNS(numofPass){
+  CharsNS=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", 
+  "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+   "r", "s", "t", "u", "v", "w", "x", "y", "z",'1','2','3','4','5','6','7','8','9','0']
+SymbolPassNS=""
+for(var i=0; i<=numofPass; i++){
+  SymbolPassNS=SymbolPassNS+CharsNS[Math.floor(Math.random() * CharsNS.length) ]
+}
+console.log(SymbolPassNS);
+return SymbolPassNS;
+}
 
 
 
